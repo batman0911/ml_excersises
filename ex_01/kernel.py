c313ee021998b6536f3048a8f77ee4e0457773e8
Linh Nguyen <linhnm>
2022-07-28 18:43:36 +0700

    refactor

--- ex_01/kernel.py
+++ ex_01/kernel.py
@@ -1,31 +1,33 @@
 import numpy as np
 
 
+def identity_kernel(X):
+    print(f'return the identity kernel matrix')
+    return np.dot(X, X.T)
+
+
+def kij_gaussian(x, y, sigma):
+    return np.exp(- 0.5 * np.dot(x - y, (x - y).T) / sigma ** 2)
+
+
+def gaussian_kernel(X):
+    print(f'return the gaussian kernel matrix')
+    N = X.shape[0]
+    sigma = 5 * np.sqrt(6)
+    K = np.zeros((N, N))
+    tol = 1e-6
+    for i in range(N):
+        for j in range(N):
+            k = kij_gaussian(X[i, :], X[j, :], sigma)
+            if abs(k) < tol:
+                k = 0
+            K[i, j] = K[j, i] = np.around(k, decimals=4)
+    return K
+
+
 class Kernel:
     def __int__(self,
                 kernel_type=None,
                 input_matrix=None):
         self.kernel_type = kernel_type
         self.input_matrix = input_matrix
-
-    def identity_kernel(self, X):
-        print(f'return the identity kernel matrix')
-        return np.dot(X, X.T)
-
-    def gaussian_kernel(self, X):
-        print(f'return the gaussian kernel matrix')
-        N = X.shape[0]
-        sigma = 5 * np.sqrt(6)
-        K = np.zeros((N, N))
-        tol = 1e-6
-        for i in range(N):
-            for j in range(N):
-                k = self.kij_gaussian(X[i, :], X[j, :], sigma)
-                if abs(k) < tol:
-                    k = 0
-                K[i, j] = K[j, i] = np.around(k, decimals=4)
-        return K
-
-    @classmethod
-    def kij_gaussian(cls, x, y, sigma):
-        return np.exp(- 0.5 * np.dot(x - y, (x - y).T) / sigma ** 2)
--- ex_01/main.py
+++ ex_01/main.py
@@ -1,9 +1,8 @@
 import numpy as np
-from sklearn import datasets
-import pandas as pd
-from kernel import Kernel
 
-# np.set_printoptions(precision=10)
+import kernel as kn
+
+np.set_printoptions(precision=3)
 
 np.random.seed(100)
 
@@ -13,9 +12,8 @@ if __name__ == '__main__':
     input_matrix = np.random.randint(0, 100, (10, 3))
     print(input_matrix)
 
-    kernel = Kernel()
-    K_identity = kernel.identity_kernel(input_matrix)
+    K_identity = kn.identity_kernel(input_matrix)
     print(f'identity kernel matrix: \n {K_identity}')
 
-    K_gaussian = kernel.gaussian_kernel(input_matrix)
+    K_gaussian = kn.gaussian_kernel(input_matrix)
     print(f'gaussian kernel matrix: \n {K_gaussian}')
