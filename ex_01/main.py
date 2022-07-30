7639634a7a08cb73a9c2022ba781a807fb6b6aee
Linh Nguyen <linhnm>
2022-07-30 15:39:56 +0700

    refactor

--- ex_01/main.py
+++ ex_01/main.py
@@ -7,7 +7,6 @@ np.set_printoptions(precision=3)
 np.random.seed(100)
 
 if __name__ == '__main__':
-    print("hello dai ca Linh")
 
     input_matrix = np.random.randint(0, 100, (10, 3))
     print(input_matrix)
